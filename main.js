faeeffe68e2bcc5899f28ac190d2193a7aa6eb65
cabrah13 <charlottemabraham32@gmail.com>
2025-12-22 18:10:36 -0500

    finished game with more improvements soon

--- main.js
+++ main.js
@@ -1,1 +1,92 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+const stringRock = "rock";
+const stringPaper = "paper";
+const stringScissors = "scissors";
+
+function getComputerChoice() {
+
+    const randomVal = Math.floor(Math.random() * 3);
+
+    switch(randomVal) {
+        case 0:
+            return stringRock;
+        case 1:
+            return stringPaper;
+        case 2: 
+            return stringScissors;
+        default: 
+            return "";
+    }
+}
+
+function getHumanChoice() {
+
+    let userInput = prompt("Rock, Paper, or Scissors?: ").toLowerCase();
+
+    switch(userInput) {
+        case "rock":
+            return stringRock;
+        case "scissors":
+            return stringScissors;
+        case "paper":
+            return stringPaper;
+        default:
+            return "";
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+
+    if (!humanChoice) {
+        console.log("Invalid choice");
+        return;
+    }
+
+    let result = "";
+    
+    if(humanChoice === computerChoice) {
+        result = "It's a tie!";
+    }else{
+        switch(humanChoice) {
+            case stringRock:
+                if(computerChoice === stringScissors) {
+                    result = "You win!";
+                    humanScore++;
+                }else {
+                    result = "You lose!";
+                    computerScore++;
+                }
+                break;
+            case stringPaper:
+                if(computerChoice === stringRock) {
+                    result = "You win!";
+                    humanScore++;
+                }else {
+                    result = "You lose!";
+                    computerScore++;
+                }
+                break;
+            case stringScissors:
+                if(computerChoice === stringPaper) {
+                    result = "You win!";
+                    humanScore++;
+                }else {
+                    result = "You lose!";
+                    computerScore++;
+                }
+                break;
+        }
+    }
+
+    console.log(result);
+    console.log(`Computer Choice: ${computerChoice}`);
+    console.log(`Human Choice: ${humanChoice}`);
+    console.log(`Your score: ${humanScore}`);
+    console.log(`Computer score: ${computerScore}`);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
